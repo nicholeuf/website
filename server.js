df820a615decd658e9484ed306518645b2788a4b
nichole <nichole.frey@doortodoororganics.com>
2017-12-07 18:03:31 -0500

    Add express server

---
+++ server.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const compression = require('compression');
+const express = require('express');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(function enforceSecure(req, res, next) {
+  if (req.get('X-Forwarded-Proto') !== 'https') {
+    return res.redirect('https://' + req.get('Host') + req.url);
+  }
+
+  return next();
+});
+
+// compress all responses
+app.use(compression())
+app.use(express.static('public'));
+
+app.get('*', function response(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, '0.0.0.0', function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Application listening on port %s', port);
+});
